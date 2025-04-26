b619698c4939203dbd7b96b7f4ec68cb68eb63cf
yohans <emailyyohans@gmail.com>
2025-04-26 14:41:23 +0300

    finished commiting

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,72 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div
+        class="d-flex justify-center align-center bg-slate300"
+        style="height: 100vh; width: 100vw"
+    >
+        <v-card
+            max-width="375"
+            class="d-flex flex-column ga-6 pa-4 bottom-shadow"
+            style="border-radius: 20px"
+        >
+            <v-img :src="qrCode" cover
+                style="border-radius: 12px"
+            ></v-img>
+
+            <div class="mb-7">
+                <div
+                    class="text-center font-weight-bold font-outfit mx-6 mb-6"
+                    style="font-size: 24px; line-height: 30px"
+                >
+                    Improve your front-end skills by building projects
+                </div>
+
+                <div
+                    class="text-center text-medium-emphasis font-outfit mx-6"
+                    style="font-size: 18px"
+                >
+                    Scan the QR code to visit Frontend Mentor and take your
+                    coding skills to the next level
+                </div>
+            </div>
+        </v-card>
+    </div>
 </template>
 
+<script>
+// import from an image
+import qrCode from './assets/image-qr-code.png'
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            qrCode
+        }
+    }
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.attribution {
+    font-size: 11px;
+    text-align: center;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.attribution a {
+    color: hsl(228, 45%, 44%);
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.bg-slate300 {
+    background-color: hsl(212, 45%, 89%);
+}
+
+.font-outfit {
+    font-family: 'Outfit', sans-serif;
+    font-optical-sizing: auto;
+}
+
+.bottom-shadow {
+    box-shadow: 
+        0 0 0 2px white,
+        0.2em 0.3em 3em rgb(100 0 0 / 10%);
+
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        themes: {
+          light: {
+            slate300: '#d6e2f0',
+            slate500: '#68778d',
+            slate900: '#1f3251', 
+          },
+        },
+      },
+})
+
+createApp(App)
+    .use(vuetify)
+    .mount('#app')
